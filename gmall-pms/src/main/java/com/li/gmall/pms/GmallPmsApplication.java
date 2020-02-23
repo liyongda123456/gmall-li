27b7f479d1abe406898b78fafe7a2c934cfcc351
Allen <1931689059@qq.com>
2020-02-23 12:56:06 +0800

    逆向完成...

--- gmall-pms/src/main/java/com/li/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/li/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,12 @@
-package com.li.gmall.gmall.pms;
+package com.li.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDubbo
+@MapperScan(basePackages = "com.li.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
